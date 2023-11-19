db9cd62aeb832948cc9483171fff77bbe6908aaa
Mohamed Abdelaziz <mohamed.abdelaziz@tutanota.de>
2023-11-20 00:39:13 +0100

    white spaces

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,6 @@ import * as vscode from 'vscode';
 import { createPackage } from './commands/createPackage';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('ros2-gde.create-package', createPackage)
+	let disposable = vscode.commands.registerCommand('ros2-gde.create-package', createPackage)
 	context.subscriptions.push(disposable);
 }
